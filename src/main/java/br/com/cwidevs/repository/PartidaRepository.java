ff6b7f02e903850fc261571f38eb75cff9df52c1
Pedro Guilherme Rohr <pedro.guilherme.rohr@gmail.com>
2017-04-20 16:53:00 -0300

    Created method on PartidaRepository to check if a jogador already scored. This method is now called in JogadorResource

--- src/main/java/br/com/cwidevs/repository/PartidaRepository.java
+++ src/main/java/br/com/cwidevs/repository/PartidaRepository.java
@@ -3,6 +3,7 @@ package br.com.cwidevs.repository;
 import br.com.cwidevs.domain.Partida;
 import br.com.cwidevs.dto.Goleador;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -14,5 +15,7 @@ public interface PartidaRepository extends JpaRepository<Partida, Long> {
 
     @Query("select new br.com.cwidevs.dto.Goleador(j.id, sum(pj.gols)) from PartidaJogador pj join pj.id.jogador j group by j")
     public List<Goleador> getGoleadores();
-    
+
+    Optional<Partida> findOneByJogadoresGolsIdJogador(long idJogador);
+
 }
